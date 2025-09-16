e052087223d48efc0dc39f4ffa45ef21f049b45d
Ryan <9024640@student.zadkine.nl>
2025-09-16 16:00:12 +0200

    .

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Book from './components/Book'
 
 function App() {
 
   return (
     <>
+      <Book />
       <h1>Opdracht 1 - Books</h1>
     </>
   )
---
+++ opdracht-01-books/src/components/Book.jsx
@@ -0,0 +1,10 @@
+const Book = () => {
+
+
+    //Javacript
+    return ( 
+        <h1>BOeknaam 1</h1>
+     );
+}
+ 
+export default Book ;
