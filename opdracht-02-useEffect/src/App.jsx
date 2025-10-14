a556ef8c18b38a18000252eabd9ffcff8d2d5e86
Ryan <9024640@student.zadkine.nl>
2025-10-14 12:59:22 +0200

    opdracht 3

--- opdracht-02-useEffect/src/App.jsx
+++ opdracht-02-useEffect/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import Clock from "./components/Clock"; // importeer de Clock-component
+import Clock from "./components/Clock";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div className="App">
       <h1>Mijn Klok App</h1>
+      <WelcomeMessage />
       <Clock /> {}
     </div>
   );
---
+++ opdracht-02-useEffect/src/components/WelcomeMessage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+function WelcomeMessage() {
+
+  const [message, setMessage] = useState("");
+
+
+  useEffect(() => {
+    const currentTime = new Date();    
+    const hours = currentTime.getHours();
+    let newMessage = "";
+
+    if (hours < 12) {
+      newMessage = "Goedemorgen!";
+    } else if (hours < 18) {
+      newMessage = "Goedemiddag!";
+    } else {
+      newMessage = "Goedenavond!";
+    }
+
+    setMessage(newMessage);
+  }, []);
+
+
+  return (
+    <div>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
