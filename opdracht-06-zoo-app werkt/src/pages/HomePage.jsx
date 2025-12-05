cf7b09cdad9d3f3ae92391b953607c23063f1f41
Ryan <9024640@student.zadkine.nl>
2025-12-05 10:18:42 +0100

    zoek 6

--- opdracht-06-zoo-app werkt/src/pages/HomePage.jsx
+++ opdracht-06-zoo-app werkt/src/pages/HomePage.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
 import { animals } from "../data/animals";
 import "./HomePage.css";
 
 export default function HomePage() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const filteredAnimals = animals.filter((animal) =>
+    animal.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <div className="page">
       <h1>Dierentuin - Dieren</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Zoek een dier"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
+
       <div className="card-list">
-        {animals.map((a) => (
+        {filteredAnimals.map((a) => (
           <Card key={a.id} animal={a} />
         ))}
       </div>
