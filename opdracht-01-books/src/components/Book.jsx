ad7bb767d50bbe038cd0584ead9cea0ff94a2014
Ryan <9024640@student.zadkine.nl>
2025-12-10 11:59:27 +0100

    13.1

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-const Book = ({ img, title, author, category}) => {
+
+const Book = ({ img, title, author, category }) => {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
 
   return (
     <section className="book-container">
@@ -7,7 +13,16 @@ const Book = ({ img, title, author, category}) => {
       <h2>{title}</h2>
       <h3>{author}</h3>
       <p>{category}</p>
+
+      <div className="favorite-section">
+        <button onClick={toggleLike} className="heart-button">
+          {liked ? "❤️" : "🤍"}
+        </button>
+
+        {liked && <p className="favorited-text">Toegevoegd aan favorieten</p>}
+      </div>
     </section>
   );
 };
+
 export default Book;
